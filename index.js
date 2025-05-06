d0fd4232edd8641266e36a5fb40fc6ff06519b31
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-05-06 12:01:52 +0100

    final edit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function myFunction(event) {
-    event.preventDefault();
-    const menu= document.getElementById("mobileMenu");
-    menu.classList.toggle("active");
-  }
- 
-  
+  event.preventDefault();
+  const menu= document.getElementById("mobileMenu");
+  menu.classList.toggle("active");
+}
+
+function closeMenu() {
+  const mobileMenu = document.getElementById("mobileMenu");
+  mobileMenu.classList.remove("active");
+
+}
